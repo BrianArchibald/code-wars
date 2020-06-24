d04ba5739dacce8837d49218c1a8ba2734bdee36
Brian Archibald <brianallenarchibald@gmail.com>
2020-06-23 21:17:31 -0500

    will u make it, return part of array

--- code-wars.py
+++ code-wars.py
@@ -30,5 +30,13 @@ def greet(language):
         'welsh': 'Croeso'
     }.get(language, 'Welcome')
 
+#  Create a method take that accepts a list/array and a number n, and returns a list/array array of the first n elements from the list/array.
 
+def take(arr,n):
+    return arr[:n]
+
+#  You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left. Considering these factors, write a function that tells you if it is possible to get to the pump or not. Function should return true (1 in Prolog) if it is possible and false (0 in Prolog) if not. The input values are always positive.
+
+def zeroFuel(distance_to_pump, mpg, fuel_left):
+    return distance_to_pump <= mpg * fuel_left
 
