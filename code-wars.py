a479a66710bd9a0187ca4145e2143a3ee7f72021
Brian Archibald <brianallenarchibald@gmail.com>
2020-06-25 21:23:40 -0500

    max number on bus

--- code-wars.py
+++ code-wars.py
@@ -49,3 +49,13 @@ def find_short(s):
 
 def divisible_by(numbers, divisor):
     return [x for x in numbers if x%divisor == 0]
+
+#  You have to write a function that accepts three parameters:
+
+cap is the amount of people the bus can hold excluding the driver.
+on is the number of people on the bus.
+wait is the number of people waiting to get on to the bus.
+If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+def enough(cap, on, wait):
+    return max(0, wait - (cap - on))
