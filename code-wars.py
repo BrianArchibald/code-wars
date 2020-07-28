da4c823a433a19ffd04396dd1a317f309a6f371d
Brian Archibald <brianallenarchibald@gmail.com>
2020-07-27 20:45:05 -0500

    reverse list, division

--- code-wars.py
+++ code-wars.py
@@ -86,3 +86,13 @@ def remove_char(s):
 
 def count_sheeps(arrayOfSheeps):
   return arrayOfSheeps.count(True)
+
+#  Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+#  You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+def litres(time):
+    return time // 2
+
+# Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+def invert(lst):
+    return [-x for x in lst]
