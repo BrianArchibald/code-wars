107fbe42a000d2fcf0fa29698a381626a3fa45da
Brian Archibald <brianallenarchibald@gmail.com>
2020-08-03 20:34:40 -0500

    return area or perimeter

--- code-wars.py
+++ code-wars.py
@@ -118,5 +118,13 @@ def say_hello(name):
 def capitalize_word(word):
     return word.capitalize()
 
+# You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+# If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+def area_or_perimeter(l , w):
+    if l == w:
+        return l * w
+    return l * 2 + w * 2
+
 
 
