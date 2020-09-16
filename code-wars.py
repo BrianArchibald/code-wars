08eab7faa0b0317cb7f8d50a6d3be2f57e17bf74
Brian Archibald <brian.archibald@imtapps.com>
2020-09-16 15:04:06 -0500

    Refactor:  to string and velocity

--- code-wars.py
+++ code-wars.py
@@ -246,5 +246,21 @@ def shark(pontoon_distance, shark_distance, you_speed, shark_speed, dolphin):
 
     return "Shark Bait!" if you_safe_time > shark_eat_time else "Alive!"
 
+# Implement a function which convert the given boolean value into its string representation.
+
+def boolean_to_string(b):
+    return str(b)
+
+# When it's spring Japanese cherries blossom, it's called "sakura" and it's admired a lot. The petals start to fall in late April.
+# Suppose that the falling speed of a petal is 5 centimeters per second (5 cm/s), and it takes 80 seconds for the petal to reach the ground from a certain branch.
+# Write a function that receives the speed (in cm/s) of a petal as input, and returns the time it takes for that petal to reach the ground from the same branch.
+# Notes:
+# The movement of the petal is quite complicated, so in this case we can see the velocity as a constant during its falling.
+# Pay attention to the data types.
+# If the initial velocity is non-positive, the return value should be 0
+
+def SakuraFall(v):
+    return 400/v if v > 0 else 0
+
 
 
