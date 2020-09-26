1b27da72e64ea7058070986d57eb323da8d5e46c
Brian Archibald <brianallenarchibald@gmail.com>
2020-09-26 16:12:50 -0500

    Refactor: min max of list

--- code-wars.py
+++ code-wars.py
@@ -312,3 +312,11 @@ def summation(num):
 
 def century(year):
     return (year + 99) // 100
+
+# Your task is to make two functions, max and min (maximum and minimum in PHP and Python) that take a(n) array/vector of integers list as input and outputs, respectively, the largest and lowest number in that array/vector.
+
+def minimum(arr):
+    return min(arr)
+
+def maximum(arr):
+    return max(arr)
