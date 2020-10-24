a11f07c158f5650cc275404276abeec88f08a916
Brian Archibald <brianallenarchibald@gmail.com>
2020-10-24 11:00:37 -0500

    Refactor: Sum unique elements of array

--- code-wars.py
+++ code-wars.py
@@ -340,3 +340,9 @@ def other_angle(a, b):
 # super_size(12)     # 21
 def super_size(n):
     return int(''.join(sorted(str(n), reverse = True)))
+
+# In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+# For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+
+def repeats(arr):
+    return sum([x for x in arr if arr.count(x) == 1])
