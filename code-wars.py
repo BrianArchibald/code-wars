74d741e602b0e5db9f641dac64525ea3e82fc22b
Brian Archibald <brianallenarchibald@gmail.com>
2020-11-07 20:29:42 -0600

    Refactor: Return number * 8 if even * 9 if odd

--- code-wars.py
+++ code-wars.py
@@ -360,3 +360,8 @@ def sum_of_n(n):
 
 def sum_mix(arr):
     return sum([int(i) for i in arr])
+
+# This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+def simple_multiplication(number) :
+    return number * 9 if number % 2 else number * 8
