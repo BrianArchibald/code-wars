a831849b9e60b81545baa9731a54fca2b5d5a06f
Brian Archibald <brianallenarchibald@gmail.com>
2020-11-14 16:14:25 -0600

    set alarm, two params(employed, vaction)

--- code-wars.py
+++ code-wars.py
@@ -376,3 +376,8 @@ def odd_count(n):
 def lovefunc(flower1, flower2):
     return flower1 % 2 != flower2 % 2
 
+#  Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+#  The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. 
+
+def set_alarm(employed, vacation):
+    return employed and not vacation
