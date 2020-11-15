2b440ace166f533f7a7494016591e5592b96587f
Brian Archibald <brianallenarchibald@gmail.com>
2020-11-15 10:16:46 -0600

    Refactor: Check two arrays and if items equal add a score

--- code-wars.py
+++ code-wars.py
@@ -377,7 +377,22 @@ def lovefunc(flower1, flower2):
     return flower1 % 2 != flower2 % 2
 
 #  Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
-#  The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. 
+#  The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise.
 
 def set_alarm(employed, vacation):
     return employed and not vacation
+
+# The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+# The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+# If the score < 0, return 0.
+def check_exam(arr1,arr2):
+    score = 0
+    for i in range(0,4):
+        if arr1[i] == arr2[i]:
+            score += 4
+        elif arr1[i] == "" or arr2[i] == "":
+            score += 0
+        else:
+            score -= 1
+
+    return score if score >= 0  else 0
