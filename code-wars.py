09647142c5dbece45dc7c90abf197e234e11e27b
Brian Archibald <brianallenarchibald@gmail.com>
2020-11-25 16:54:20 -0600

    Refactor: Multiplication table for number arg

--- code-wars.py
+++ code-wars.py
@@ -412,3 +412,7 @@ def is_uppercase(inp):
 def triple_trouble(one, two, three):
     return "".join(a+b+c for a,b,c in zip(one,two,three))
 
+# Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+def multi_table(number):
+    return '\n'.join(f'{i} * {number} = {i * number}' for i in range(1, 11))
+
