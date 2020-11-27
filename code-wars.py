6c769eb70ee24fa3e8f0c36bf0363b16d0bd8995
Brian Archibald <brianallenarchibald@gmail.com>
2020-11-27 15:13:28 -0600

    Refactor: get pow of array by n then subtract sum of array

--- code-wars.py
+++ code-wars.py
@@ -426,3 +426,12 @@ def multi_table(number):
 def correct(string):
     return string.replace('5', 'S').replace('0', 'O').replace('1', 'I')
 
+# You are provided with an array of positive integers and an additional integer n (n > 1).
+# Calculate the sum of each value in the array to the nth power. Then subtract the sum of the original array.
+# Examples
+# {1, 2, 3}, 3  -->  (1^3 + 2^3 + 3^3 ) - (1 + 2 + 3)  -->  36 - 6  -->  30
+# {1, 2}, 5     -->  (1^5 + 2^5) - (1 + 2)             -->  33 - 3  -->  30
+
+def modified_sum(a, n):
+    return sum([item**n for item in a]) - sum([item for item in a])
+
