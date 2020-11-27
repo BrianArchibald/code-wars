a870913c00c2ccb7afbedfa45403d6bd25968824
Brian Archibald <brianallenarchibald@gmail.com>
2020-11-26 18:23:24 -0600

    Refactor: Replacing ints w string

--- code-wars.py
+++ code-wars.py
@@ -416,3 +416,13 @@ def triple_trouble(one, two, three):
 def multi_table(number):
     return '\n'.join(f'{i} * {number} = {i * number}' for i in range(1, 11))
 
+# Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+# When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+# Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+# S is misinterpreted as 5
+# O is misinterpreted as 0
+# I is misinterpreted as 1
+# The test cases contain numbers only by mistake.
+def correct(string):
+    return string.replace('5', 'S').replace('0', 'O').replace('1', 'I')
+
